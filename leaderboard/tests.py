15e84e7223ac0b77e9ced23621b3229db5a4d1fe
jayvanput <jayvanput@gmail.com>
2022-01-24 00:32:28 -0500

    Added basic unit tests for home_page

--- leaderboard/tests.py
+++ leaderboard/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import resolve
 
+from leaderboard.views import home_page
 # Create your tests here.
+
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        home_view = resolve("/")
+        self.assertEqual(home_view.func, home_page)
