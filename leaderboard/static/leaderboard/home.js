f22dccc87c708dbf41b44eac8ad9172935b8918a
jayvanput <jayvanput@gmail.com>
2022-02-19 23:15:30 -0500

    Added modal form with css and js

--- leaderboard/static/leaderboard/home.js
+++ leaderboard/static/leaderboard/home.js
@@ -1,6 +1,6 @@
-let button = document.getElementById("submit_time")
-let form = document.getElementById("form")
+let button = document.getElementById("show_form")
+let form = document.getElementById("time_form")
 
 button.addEventListener("click", () => {
-    form.classList.toggle("hidden")
+    form.classList.toggle("shown")
 })
